5aefbe6947f2498f71b09826bd7094803275801c
zeasnelson <zeasnelson@gmail.com>
2020-04-14 21:07:58 -0400

    error

--- src/Clock.java
+++ src/Clock.java
@@ -10,35 +10,6 @@ public class Clock {
        startTime = System.currentTimeMillis();
     }
 
-//    public static Long millisToHour(Long milli){
-//        if( milli == null || milli == 0 ){
-//            return (long)0;
-//        }
-//        return TimeUnit.MILLISECONDS.toHours(milli);
-//    }
-//
-//    public static Long millisToMinutes(Long milli){
-//        if( milli == null || milli == 0 ){
-//            return (long)0;
-//        }
-//        return TimeUnit.MILLISECONDS.toMinutes(milli);
-//    }
-//
-    public static Long millisToSecs(Long milli){
-
-        if( milli == null || milli == 0 ){
-            return (long)0;
-        }
-        return TimeUnit.MILLISECONDS.toSeconds(milli);
-    }
-
-
-//    public static String getHHMMSS(Long elapsedTime){
-//        Long hour = millisToHour(elapsedTime);
-//        Long min = millisToMinutes(elapsedTime);
-//        Long secs = millisToSecs(elapsedTime);
-//        return String.format("%02d:%02d:%02d", hour, min, secs);
-//    }
 
     public static void setTimerStart(long length){
         timerLength = length;
